abd2b010186fb219f826f29e78399af1ad9ea2eb
Sergey Ratiashvili <serg.s.r@gmail.com>
2023-08-07 19:24:07 +0200

    feat: proper export

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,31 @@ import { CosmoparkConfig } from './types';
 import { Cosmopark } from './cosmopark';
 import fs from 'fs';
 
-program
-  .name('cosmopark')
-  .description('CLI to start your own cosmos')
-  .command('start')
-  .argument('<config>', 'config file path, may me toml or json')
-  .action(async (str) => {
-    let config: CosmoparkConfig;
-    if (str.endsWith('.toml')) {
-      config = toml.parse(fs.readFileSync(str, 'utf-8')) as CosmoparkConfig;
-    } else if (str.endsWith('.json')) {
-      config = JSON.parse(fs.readFileSync(str, 'utf-8'));
-    } else {
-      console.log('Unknown file format');
-    }
-    console.log('🚀 Starting');
-    try {
-      await Cosmopark.create(config);
-    } catch (e) {
-      console.log('ERROR', e);
-    }
-    console.log('🥳 Done');
-  });
+if (require.main === module) {
+  program
+    .name('cosmopark')
+    .description('CLI to start your own cosmos')
+    .command('start')
+    .argument('<config>', 'config file path, may me toml or json')
+    .action(async (str) => {
+      let config: CosmoparkConfig;
+      if (str.endsWith('.toml')) {
+        config = toml.parse(fs.readFileSync(str, 'utf-8')) as CosmoparkConfig;
+      } else if (str.endsWith('.json')) {
+        config = JSON.parse(fs.readFileSync(str, 'utf-8'));
+      } else {
+        console.log('Unknown file format');
+      }
+      console.log('🚀 Starting');
+      try {
+        await Cosmopark.create(config);
+      } catch (e) {
+        console.log('ERROR', e);
+      }
+      console.log('🥳 Done');
+    });
 
-program.parse();
+  program.parse();
+}
+
+export { Cosmopark };
